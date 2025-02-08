02a42d485865db04024f2792ba26be83e65a6819
Arun Patro <arun.kumar.patro@gmail.com>
2025-02-08 13:10:42 -0500

    save

--- init.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def init(name):
-    print(f"Initializing project {name}")
-    os.mkdir(name)
-    os.mkdir(f"{name}/frontend")
-    os.mkdir(f"{name}/supabase")
-    raise NotImplementedError("Not implemented")
